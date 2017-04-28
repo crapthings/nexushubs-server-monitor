bb7b0c5f27fdb5d59c302116397f6f50701fd1bf
crapthings <crapthings@gmail.com>
2017-04-28 18:28:23 +0800

    minor update

--- nsm-dashboard/client/main.js
+++ nsm-dashboard/client/main.js
@@ -1,22 +1,7 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { render } from 'react-dom'
 
-import './main.html';
+Meteor.startup(function () {
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+  render(<h1>hello</h1>, document.getElementById('app'))
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+})
